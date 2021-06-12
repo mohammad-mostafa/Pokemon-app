b3946d397020b030bbea3f3c3f6f8516e768c25a
mohammad Mostafa <mohammad.mostafa.m@gmail.com>
2021-06-12 21:05:41 +0400

    fixed the load method to read the offset from the next url

--- app/src/main/java/com/mo/pokeapp/data/dto/SpeciesListResponse.kt
+++ app/src/main/java/com/mo/pokeapp/data/dto/SpeciesListResponse.kt
@@ -2,6 +2,7 @@ package com.mo.pokeapp.data.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class SpeciesListResponse(@SerializedName("results") val list: List<SpeciesResult>) {
-
-}
+data class SpeciesListResponse(
+    @SerializedName("next") val nextUrl: String?,
+    @SerializedName("results") val list: List<SpeciesResult>
+)
