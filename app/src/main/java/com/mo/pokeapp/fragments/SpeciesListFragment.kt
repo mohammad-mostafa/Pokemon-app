5ca1f50064b4ec235a6846384ae22c39eceae9d1
mohammad Mostafa <mohammad.mostafa.m@gmail.com>
2021-06-19 00:46:52 +0400

    added a bottom sheet to show errors

--- app/src/main/java/com/mo/pokeapp/fragments/SpeciesListFragment.kt
+++ app/src/main/java/com/mo/pokeapp/fragments/SpeciesListFragment.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.mo.pokeapp.R
 import com.mo.pokeapp.adapters.SpeciesListAdapter
 import com.mo.pokeapp.adapters.SpeciesViewHolder
 import com.mo.pokeapp.core.BaseFragment
 import com.mo.pokeapp.data.viewobject.SpeciesVO
 import com.mo.pokeapp.databinding.FragmentSpeciesListBinding
+import com.mo.pokeapp.databinding.LayoutErrorBinding
 import com.mo.pokeapp.viewmodels.SpeciesListViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -24,6 +30,20 @@ class SpeciesListFragment : BaseFragment() {
     private val binding get() = _binding!!
 
     private val viewModel: SpeciesListViewModel by viewModels()
+
+    private val errorBottomSheet by lazy {
+        BottomSheetDialog(requireContext())
+            .also { bottomSheet ->
+                val layoutErrorBinding = LayoutErrorBinding.inflate(layoutInflater)
+                bottomSheet.setContentView(layoutErrorBinding.root)
+
+                bottomSheet.dismissWithAnimation = true
+
+                layoutErrorBinding.okButton.setOnClickListener {
+                    bottomSheet.dismiss()
+                }
+            }
+    }
 
     private val adapter by lazy {
         SpeciesListAdapter(object : SpeciesViewHolder.SpeciesViewHolderCallback {
@@ -53,6 +73,14 @@ class SpeciesListFragment : BaseFragment() {
             }
         })
 
+        lifecycleScope.launch {
+            adapter.loadStateFlow.collectLatest { loadStates ->
+
+                binding.progressBar.isVisible = loadStates.refresh is LoadState.Loading
+                showErrorMsg(loadStates.refresh is LoadState.Error, getString(R.string.error_loading_list))
+            }
+        }
+
         viewModel.navigateToDetails.observe(viewLifecycleOwner, {
 
             it.getContentIfNotHandled()?.let { url ->
@@ -62,6 +90,16 @@ class SpeciesListFragment : BaseFragment() {
                 findNavController().navigate(action)
             }
         })
+    }
+
+    private fun showErrorMsg(show: Boolean, msg: String) {
+
+        if (show) {
+            LayoutErrorBinding.inflate(layoutInflater).msgTv.text = msg
+            errorBottomSheet.show()
+        } else {
+            if (errorBottomSheet.isShowing) errorBottomSheet.dismiss()
+        }
     }
 
     private fun setUpRecyclerView() {
