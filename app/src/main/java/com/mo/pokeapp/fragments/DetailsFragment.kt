41103140cbf10c38dfeae51eb51ffcb6957f6062
mohammad Mostafa <mohammad.mostafa.m@gmail.com>
2021-06-19 22:53:58 +0400

    added a bottom sheet to show errors in the details screen

--- app/src/main/java/com/mo/pokeapp/fragments/DetailsFragment.kt
+++ app/src/main/java/com/mo/pokeapp/fragments/DetailsFragment.kt
@@ -8,12 +8,14 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mo.pokeapp.R
 import com.mo.pokeapp.adapters.EvolutionChainAdapter
 import com.mo.pokeapp.adapters.FlavorsAdapter
 import com.mo.pokeapp.adapters.TitleAdapter
 import com.mo.pokeapp.core.BaseFragment
 import com.mo.pokeapp.databinding.FragmentDetailsBinding
+import com.mo.pokeapp.databinding.LayoutErrorBinding
 import com.mo.pokeapp.viewmodels.SpeciesDetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -77,6 +79,29 @@ class DetailsFragment : BaseFragment() {
             evolutionChainTitleAdapter.submitList(listOf(getString(R.string.evolution_chain)))
             evolutionChainAdapter.submitList(it)
         })
+
+        viewModel.errorObservable.observe(viewLifecycleOwner, {
+            val msg = it.getContentIfNotHandled()
+            msg?.let {
+                showError(getString(msg))
+            }
+        })
+    }
+
+    private fun showError(msg: String) {
+        val bottomSheet = BottomSheetDialog(requireContext())
+
+        val layoutErrorBinding = LayoutErrorBinding.inflate(layoutInflater)
+        bottomSheet.setContentView(layoutErrorBinding.root)
+
+        bottomSheet.dismissWithAnimation = true
+
+        layoutErrorBinding.msgTv.text = msg
+
+        layoutErrorBinding.okButton.setOnClickListener {
+            bottomSheet.dismiss()
+        }
+        bottomSheet.show()
     }
 
     private fun setupRecyclerView() {
--- app/src/main/java/com/mo/pokeapp/viewmodels/SpeciesDetailsViewModel.kt
+++ app/src/main/java/com/mo/pokeapp/viewmodels/SpeciesDetailsViewModel.kt
@@ -3,11 +3,13 @@ package com.mo.pokeapp.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mo.pokeapp.R
 import com.mo.pokeapp.data.repo.EvolutionChainRepo
 import com.mo.pokeapp.data.repo.SpeciesDetailsRepo
 import com.mo.pokeapp.data.viewobject.SpeciesDetailsVO
 import com.mo.pokeapp.data.viewobject.SpeciesVO
 import com.mo.pokeapp.network.Result
+import com.mo.pokeapp.utils.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -31,6 +33,10 @@ class SpeciesDetailsViewModel @Inject constructor(
         MutableLiveData<List<SpeciesVO>>()
     }
 
+    val errorObservable by lazy {
+        MutableLiveData<Event<Int>>()
+    }
+
     private fun fetchSpeciesDetails(detailsUrl: String) {
         viewModelScope.launch {
 
@@ -39,17 +45,17 @@ class SpeciesDetailsViewModel @Inject constructor(
                     speciesDetails.value = result.data
                     result.data.evolutionChainUrl?.let { fetchEvolutionChain(it) }
                 }
-                else -> TODO()
+                else -> errorObservable.value = Event(R.string.species_details_failure)
             }
         }
     }
 
     private suspend fun fetchEvolutionChain(evolutionChainUrl: String) {
-        when(val result = evolutionChainRepo.getEvolutionChain(evolutionChainUrl)) {
+        when (val result = evolutionChainRepo.getEvolutionChain(evolutionChainUrl)) {
             is Result.Success -> {
                 evolutionChain.value = result.data
             }
-            else -> TODO()
+            else -> errorObservable.value = Event(R.string.evolution_chain_failure)
         }
     }
 
